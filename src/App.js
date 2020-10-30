c300220268c649e74a32105df82110263eec67e0
Dmytro1231 <dimarubalchenk@ukr.net>
2020-10-30 20:55:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const MyFunctionComponent = (props) => {
+  return <div>{props.text || 'default'}</div>
+};
+
+class MyClassComponent extends React.Component {
+  render() {
+    return <div>{this.props.text || 'default'}</div>
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hello world
+        </p>
+        <MyFunctionComponent text="1">
+
+        </MyFunctionComponent>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
